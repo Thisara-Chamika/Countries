806220a28396f5126c506d7e3cb78df93e65544f
Thisara-Chamika <thisarachamika2003@gmail.com>
2024-08-06 21:02:41 +0530

    Add details

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 let tblCountries = document.getElementById("tblCountries");
 
 let tableBody = `<tr>
-                    <th>Name</th>
-                    <th>Falg</th>
+                    <th><h3><b>Name</b></h3></th>
+                    <th><h3><b>Flag</b></h3></th>
                 </tr>` ;
 
 
@@ -15,7 +15,12 @@ fetch("https://restcountries.com/v3.1/all")
     data.forEach(element => {
 
         tableBody+=`<tr>
-                        <td><h3>${element.name.common}</h3><br>${element.capital}</td>
+                        <td><h5>${element.name.common}</h5><br>
+                        <b>Official- Name</b>: ${element.name.official}<br>
+                        <b>Capital</b>: ${element.capital}<br>
+                        <b>Region</b>: ${element.region}<br>
+                        <b>Continents</b>: ${element.continents}<br>
+                        <b>Population</b>: ${element.population}<br>
                         <td><img src="${element.flags.png}" alt=""></td>
                     </tr>`
         console.log(element.name.common);
